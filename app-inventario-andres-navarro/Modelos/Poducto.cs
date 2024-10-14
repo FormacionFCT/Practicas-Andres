43cb23e8029bbe89607bf8bea0ae424802bf248f
Andrés <4145808@alu365.murciaeduca.es>
2024-10-14 09:16:48 +0200

    Defino la Clase Producto

---
+++ app-inventario-andres-navarro/Modelos/Poducto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_inventario_andres_navarro.Modelos
+{
+    class Poducto
+    {
+        public int IdProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public string Cantidad { get; set; }
+        public string Precio { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
