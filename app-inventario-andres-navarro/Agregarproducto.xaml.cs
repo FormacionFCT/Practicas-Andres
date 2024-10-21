ed436c114cb6f6ff8e699dc8a373c021d47f67e2
Andrés <4145808@alu365.murciaeduca.es>
2024-10-21 10:43:57 +0200

    Implemento el método agregar_click en la interfaz de agregar producto

--- app-inventario-andres-navarro/Agregarproducto.xaml.cs
+++ app-inventario-andres-navarro/Agregarproducto.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using app_inventario_andres_navarro.Modelos;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,43 @@ namespace app_inventario_andres_navarro
     /// </summary>
     public partial class Agregarproducto : Window
     {
-        public Agregarproducto()
+        private ObservableCollection<Producto> productoList;
+
+        public Agregarproducto(ObservableCollection<Producto> productos)
         {
             InitializeComponent();
+            this.productoList = productos;
         }
+
+        private void Agregar_Click(object sender, RoutedEventArgs e)
+        {
+            //Verificar que no esten vacíos los campos
+            if (string.IsNullOrWhiteSpace(txtId.Text) ||
+                string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                string.IsNullOrWhiteSpace(txtCantidad.Text) ||
+                string.IsNullOrWhiteSpace(txtPrecio.Text) ||
+                string.IsNullOrWhiteSpace(txtDescripcion.Text))
+            {
+                MessageBox.Show("Por favor completa todos los campos.");
+                return; // No continúa si hay campos vacíos
+            }
+
+            var nuevoProducto = new Producto
+            {
+                IdProducto = int.Parse(txtId.Text),
+                NombreProducto = txtNombre.Text,
+                Cantidad = int.Parse(txtCantidad.Text),
+                Precio = txtPrecio.Text,
+                Descripcion = txtDescripcion.Text
+            };
+
+
+
+            productoList.Add(nuevoProducto);
+            MessageBox.Show("Producto agregado con éxito!");            
+            Close(); // Cerrar la ventana después de agregar
+        }
+    }
+}
     }
 }
