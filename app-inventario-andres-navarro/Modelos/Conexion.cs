72b3ffd4b149b4cdaa0a6d25e4ab2386d9c60ff4
Andrés <4145808@alu365.murciaeduca.es>
2024-10-28 10:35:16 +0100

    Instalado el nuget y establecida la conexcion con sqlite

---
+++ app-inventario-andres-navarro/Modelos/Conexion.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_inventario_andres_navarro.Modelos
+{
+    public class Conexion
+    {
+        private string conexionString = "Data Source=productos.db;Version=3;";
+
+        public Conexion()
+        {
+            CrearTablaSiNoExiste(); // Llama al método en el constructor
+        }
+
+        // Método que crea la tabla si no existe
+        private void CrearTablaSiNoExiste()
+        {
+            using (var conexion = new SQLiteConnection(conexionString))
+            {
+                conexion.Open();
+
+                //SQL para crea tabla
+                string query = @"
+                CREATE TABLE IF NOT EXISTS Producto (
+                    Id INTEGER PRIMARY KEY UNIQUE,
+                    Nombre TEXT NOT NULL,
+                    Cantidad INTEGER NOT NULL,
+                    Precio TEXT NOT NULL,
+                    Descripcion TEXT NOT NULL
+                );";
+                using (var command = new SQLiteCommand(query, conexion))
+                {
+                    command.ExecuteNonQuery(); // Ejecuta el comando
+                }
+            }
+        }
+
+        public List<Producto> GetAllProducto()
+        {
+            List<Producto> oLista = new List<Producto>();
+
+            using (var connection = new SQLiteConnection(conexionString))
+            {
+                connection.Open();
+                string query = "SELECT Id, Nombre, Cantidad, Precio, Descripcion FROM Producto";
+                SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                cmd.CommandType = System.Data.CommandType.Text;
+                {
+                    using (SQLiteDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            oLista.Add(new Producto()
+                            {
+                                IdProducto = int.Parse(dr["Id"].ToString()),
+                                NombreProducto = dr["Nombre"].ToString(),
+                                Cantidad = int.Parse(dr["Cantidad"].ToString()),
+                                Precio = dr["Precio"].ToString(),
+                                Descripcion = dr["Descripcion"].ToString(),
+                            });
+                        }
+                    }
+                }
+            }
+
+            return oLista;
+        }
+
+        public bool Editar(Producto obj)
+        {
+            bool respuesta = true;
+
+
+
+            using (var connection = new SQLiteConnection(conexionString))
+            {
+                connection.Open();
+                string query = "Update Producto set Nombre = @nombre, Cantidad = @cantidad, Precio = @precio, Descripcion = @descripcion, Pais = @pais where Id = @id";
+
+                SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                cmd.Parameters.Add(new SQLiteParameter("@id", obj.IdProducto));
+                cmd.Parameters.Add(new SQLiteParameter("@nombre", obj.NombreProducto));
+                cmd.Parameters.Add(new SQLiteParameter("@cantidad", obj.Cantidad));
+                cmd.Parameters.Add(new SQLiteParameter("@precio", obj.Precio));
+                cmd.Parameters.Add(new SQLiteParameter("@descripcion", obj.Descripcion));
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                if (cmd.ExecuteNonQuery() < 1)
+                {
+                    respuesta = false;
+                }
+            }
+
+            return respuesta;
+        }
+
+
+        public void AddProducto(Producto producto)
+        {
+            using (var connection = new SQLiteConnection(conexionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Producto (Nombre, Cantidad, Precio, Descripcion) VALUES (@nombre, @cantidad, @precio, @descripcion)";
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@nombre", producto.NombreProducto);
+                    command.Parameters.AddWithValue("@direccion", producto.Cantidad);
+                    command.Parameters.AddWithValue("@ciudad", producto.Precio);
+                    command.Parameters.AddWithValue("@pais", producto.Descripcion);
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public bool Eliminar(Producto obj)
+        {
+            bool respuesta = true;
+
+            using (var connection = new SQLiteConnection(conexionString))
+            {
+                connection.Open();
+                string query = "Delete from producto where Id = @id";
+
+                SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                cmd.Parameters.Add(new SQLiteParameter("@id", obj.IdProducto));
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                if (cmd.ExecuteNonQuery() < 1)
+                {
+                    respuesta = false;
+                }
+            }
+
+            return respuesta;
+        }
+
+    }
+
+}
--- app-inventario-andres-navarro/Modelos/ViewModel.cs
+++ app-inventario-andres-navarro/Modelos/ViewModel.cs
@@ -4,45 +4,71 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace app_inventario_andres_navarro.Modelos
 {
     public class ViewModel
     {
         public ObservableCollection<Producto> Productos { get; set; }
+        public Conexion _conexion;
 
 
         public ViewModel() 
         {
-            Productos = new ObservableCollection<Producto>
-            {
-                new()
-              {
+            _conexion = new Conexion();
 
-                    IdProducto = 1,
-                    NombreProducto = "Mesa",
-                    Cantidad = 10,
-                    Precio = "50,20$",
-                    Descripcion = "Mesas de auténtico roble"
-                },
+            var listaProductos = _conexion.GetAllProducto();
+            Productos = new ObservableCollection<Producto>(listaProductos);
 
-                 new() {
-
-                     IdProducto = 2,
-                     NombreProducto = "Silla",
-                     Cantidad = 4,
-                     Precio = "10$",
-                     Descripcion = "Sillas plegables"
-                 },
-                 new() {
-
-                     IdProducto = 3,
-                     NombreProducto = "Armario",
-                     Cantidad = 5,
-                     Precio = "100$",
-                     Descripcion = "Armarios de 2m de altura con amplio fondo"
-                 }
-            };
         }
+
+        public void AgregarProducto(Producto nuevoProducto)
+        {
+            _conexion.AddProducto(nuevoProducto);
+
+            Productos.Add(nuevoProducto);
+        }
+
+        public void EditarEmpleado(Producto producto)
+        {
+            bool exito = _conexion.Editar(producto);
+
+            if (exito)
+            {
+
+                var productoExistente = Productos.FirstOrDefault(e => e.IdProducto == producto.IdProducto);
+                if (productoExistente != null)
+                {
+                    productoExistente.NombreProducto = producto.NombreProducto;
+                    productoExistente.Cantidad = producto.Cantidad;
+                    productoExistente.Precio = producto.Precio;
+                    productoExistente.Descripcion = producto.Descripcion;
+
+                    var index = Productos.IndexOf(productoExistente);
+                    Productos[index] = productoExistente;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error al actualizar el empleado en la base de datos.");
+            }
+        }
+
+        // Método para eliminar un empleado
+        public void EliminarEmpleado(Producto producto)
+        {
+            bool exito = _conexion.Eliminar(producto);
+            if (exito)
+            {
+               Productos.Remove(producto);
+            }
+            else
+            {
+                MessageBox.Show("Error al eliminar el empleado en la base de datos.");
+            }
+        }
+
+
     }
 }
